b2de378f1a0e93951a88332b960eb73cf90885e0
LordCat <kristianalexball@gmail.com>
2024-06-01 22:37:09 +0100

    Realized I'm dumb and there's a trailing comma in the csv. Fixed.

--- js_parse_package/store_files.py
+++ js_parse_package/store_files.py
@@ -31,13 +31,20 @@ def store_urls(url):
         pass
     except AttributeError:
         pass
-   
-    for quoted_dir in extract_urls(url):
+
+    extracted_urls = extract_urls(url)
+    num_urls = len(extracted_urls)
+    i = 0
+    for quoted_dir in extracted_urls:
+        i += 1
         try:
             if (args.isolate):
                 dir = quoted_dir.strip('"')
                 with open(f"""{target["domain"]}/parsed-urls/{file_name}+dirs.txt""", "a", encoding="utf-8") as directories:
-                    directories.write(dir + ',\n')
+                    if i == num_urls:
+                        directories.write(dir + '\n')  # No comma for the last directory
+                    else:
+                        directories.write(dir + ',\n')
             elif (args.merge):
                 dir = quoted_dir.strip('"')
                 all_dirs.append(dir)
@@ -49,7 +56,6 @@ def store_urls(url):
                 parsed_files_directory_path = f"""{target["domain"]}/parsed-files/"""
                 if not (os.path.exists(parsed_files_directory_path)):
                     os.makedirs(parsed_files_directory_path)
-
 def write_files():
     remove_dupes(all_dirs)
     if (args.remove_third_parties):
